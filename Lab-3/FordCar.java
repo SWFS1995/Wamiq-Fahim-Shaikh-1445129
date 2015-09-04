87b32837ee3f7876bdf9b14e73a6cebf483fd8e6
SWFS1995 <wamiq_fahim@hotmail.com>
2015-09-05 01:25:10 +0500

    Create FordCar.java

---
+++ Lab-3/FordCar.java
@@ -0,0 +1,24 @@
+
+public class FordCar 
+{
+	public String color, type, model;
+	public int cylinder;
+	
+	public void start()
+	{
+		System.out.println("The car has been started.");
+	}
+	public void Accelerate()
+	{
+		System.out.println("Car is Accelerating.");
+	}
+	public void Reverse()
+	{
+		System.out.println("Car is backing up.");
+	}
+	public void Stop()
+	{
+		System.out.println("The car is stopping/");
+	}
+
+}
