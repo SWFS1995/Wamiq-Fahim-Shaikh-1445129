9bc882f296feb2e900ed2504380fcd34ca15e83f
SWFS1995 <wamiq_fahim@hotmail.com>
2015-09-05 01:26:05 +0500

    Create Person.java

---
+++ Lab-3/Person.java
@@ -0,0 +1,28 @@
+
+public class Person
+{
+	public String Name;
+	public int age;
+	public float height;
+	
+	public void Eat()
+	{
+		System.out.println("Eating.");
+	}
+	public void Listen()
+	{
+		System.out.println("Listening.");
+	}
+	public void Run()
+	{
+		System.out.println("Running.");
+	}
+	public void Walk()
+	{
+		System.out.println("Walking.");
+	}
+	public void Speak()
+	{
+		System.out.println("Speaking.");
+	}
+}
